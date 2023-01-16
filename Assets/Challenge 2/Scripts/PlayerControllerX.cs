712b8ab99e042465c4f60b6141764aed1b59b34c
Arthur Matias <ahmmfdc@gmail.com>
2023-01-16 09:40:58 -0300

    first commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool canSpawn = true;
+    float delayToSpawn = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawn = false;
+            Invoke("SetCanSpawn", delayToSpawn);
+        }
+    }
+    void SetCanSpawn() { canSpawn = true; }
+}
